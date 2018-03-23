76a0ac81c3e601a31d0b5528ec97da3a529ec9cc
AngelDX <angel.condori@gmail.com>
2018-03-23 11:04:11 -0500

    first commit 2

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-
-echo "<h2>TCP/IP Connection</h2>\n";
 
 /* Obtener el puerto para el servicio WWW. */
 $service_port = '7654';
@@ -11,33 +8,15 @@ $address = '172.22.90.1';
 
 /* Crear un socket TCP/IP. */
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if ($socket === false) {
-    echo "socket_create() falló: razón: " . socket_strerror(socket_last_error()) . "\n";
-} else {
-    echo "OK.\n";
-}
 
-echo "Intentando conectar a '$address' en el puerto '$service_port'...";
 $result = socket_connect($socket, $address, $service_port);
-if ($result === false) {
-    echo "socket_connect() falló.\nRazón: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
-}
 
-$in = "HEAD / HTTP/1.1\r\n";
-$in .= "Host: www.example.com\r\n";
+
+$in = "HEAD mi texto\r\n";
+$in .= "Host: cliente php\r\n";
 $in .= "Connection: Close\r\n\r\n";
-$out = '';
 
-echo "Enviando petición HTTP HEAD ...";
 socket_write($socket, $in, strlen($in));
-echo "OK.\n";
-
-echo "Leyendo respuesta:\n\n";
-while ($out = socket_read($socket, 2048)) {
-    echo $out;
-}
 
 echo "Cerrando socket...";
 socket_close($socket);
